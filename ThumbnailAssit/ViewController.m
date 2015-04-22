be3884c83dac5735384e2f1f664ee86cc323d7bf
Jerry Edens <jerry.edens@gmail.com>
2015-04-22 01:50:59 -0700

    Initial Commit

---
+++ ThumbnailAssit/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ThumbnailAssit
+//
+//  Created by Jerry Edens on 4/22/15.
+//  Copyright (c) 2015 Jerry Edens. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ ThumbnailAssit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ThumbnailAssit
+//
+//  Created by Jerry Edens on 4/22/15.
+//  Copyright (c) 2015 Jerry Edens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
